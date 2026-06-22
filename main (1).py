6d27040d419d08db8992d4a581c58ce75fc8b6f1
Pedro Nunes Valeriano Duarte <125411554+PedrokaIsACoder@users.noreply.github.com>
2026-06-22 19:01:09 -0300

    Adicionando alguns comentarios adicionais

--- main (1).py
+++ main (1).py
@@ -2,11 +2,11 @@ import math
 
 # Função para calcular integral
 def CalcularIntegral(F, a, b):
-    return F(b) - F(a)
+    return F(b) - F(a)                  # Teorema Fundamental do Calculo
     
 # f(x) = x^2
 def F1(x):
-    return (x**3)/3
+    return (x**3)/3                     # Retornar para o valor da integral
     
 # f(x) = sen(x)
 def F2(x):
@@ -15,7 +15,8 @@ def F2(x):
 # f(x) = e^x  
 def F3(x):
     return math.exp(x)
-    
+
+# Exibir o valor já com o resultado da integral definida no devido intervalo
 print("Integral de x^2 no intervalo [0,1]: ", CalcularIntegral(F1, 0, 1))
 print("Integral de sen(x) no intervalo [0,1]: ", CalcularIntegral(F2, 0, math.pi))
 print("Integral de e^x no intervalo [0,1]: ", CalcularIntegral(F3, 0, 1))
